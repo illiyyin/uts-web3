95275af4b62487b7d0e6fc1de00b3614e3069a6d
illiyyin <iilliyyin@gmail.com>
2020-04-15 16:01:05 +0700

    Updates

---
+++ precache-manifest.e0b8285cfcd40ebbb7899bdf98c229d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e86b58a6dd750a5895f86237125b8ce",
+    "url": "/uts-web3/index.html"
+  },
+  {
+    "revision": "f2cc661a93cf73e37fd6",
+    "url": "/uts-web3/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "cca01389d084e6808839",
+    "url": "/uts-web3/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f2cc661a93cf73e37fd6",
+    "url": "/uts-web3/static/js/2.48b0e722.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uts-web3/static/js/2.48b0e722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca01389d084e6808839",
+    "url": "/uts-web3/static/js/main.4c0c0007.chunk.js"
+  },
+  {
+    "revision": "2459a651da10c995a411",
+    "url": "/uts-web3/static/js/runtime-main.0c77138c.js"
+  }
+]);
